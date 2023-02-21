a16e0860bd420e66ffc898611566db8003078ef5
Josh Maxwell <jmaxwell@qualtrics.com>
2023-02-20 23:07:49 -0700

    adding autocmds for yank highlighting and relativenumber disabling when leaving buffer

---
+++ nvim/lua/jmaxwell/autocmd.lua
@@ -0,0 +1,30 @@
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- [[ Disable relativenumber when leaving a buffer ]]
+local toggleRelativenumber = vim.api.nvim_create_augroup('toggleRelativenumber', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter' }, {
+  desc = 'Enable relative line numbers when entering a buffer',
+  group = toggleRelativenumber,
+  callback = function ()
+    vim.wo.relativenumber = true
+  end
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave' }, {
+  desc = 'Remove relative line numbers when leaving a buffer',
+  group = toggleRelativenumber,
+  callback = function ()
+    vim.wo.relativenumber = false
+  end
+})
+
