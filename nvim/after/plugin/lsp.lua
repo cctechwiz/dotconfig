958ddda0c9996408a4cdd77f9797d3527bd587c6
Josh Maxwell <cctechwiz@gmail.com>
2023-09-14 22:10:36 -0600

    fixing vim lua bug

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -11,34 +11,34 @@ lsp.ensure_installed({
 })
 
 --neodev replaces the following config block but only in neovim lua configs
-require('neodev').setup()
+--require('neodev').setup()
 -- Fix Undefined global 'vim'
---lsp.configure('lua_ls', {
---  --on_attach = function(client, bufnr)
---    --    print('hello lua-language-server')
---    --end,
---    settings = {
---      Lua = {
---        runtime = {
---          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
---          version = 'LuaJIT',
---        },
---        diagnostics = {
---          -- Get the language server to recognize the `vim` global
---          globals = {'vim'},
---        },
---        workspace = {
---          -- Make the server aware of Neovim runtime files
---          library = vim.api.nvim_get_runtime_file("", true),
---          checkThirdParty = false,
---        },
---        -- Do not send telemetry data containing a randomized but unique identifier
---        telemetry = {
---          enable = false,
---        },
---      }
---    }
---})
+lsp.configure('lua_ls', {
+  --on_attach = function(client, bufnr)
+    --    print('hello lua-language-server')
+    --end,
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+         version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      }
+    }
+})
 
 
 local cmp = require('cmp')
