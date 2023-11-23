81b76def353bc68d0b1c30eee56ee378764533e7
Josh Maxwell <cctechwiz@gmail.com>
2023-11-23 07:59:27 -0700

    Adding needed languages for vue support

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -14,6 +14,10 @@ require'nvim-treesitter.configs'.setup {
     "typescript",
     "vim",
     "vimdoc",
+    "vue",
+    "html",
+    "css",
+    "scss",
   },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -24,7 +28,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+  --ignore_install = { "javascript" },
 
   highlight = {
     enable = true,
