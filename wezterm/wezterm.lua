0bb8d2c3bba16cc9deebfc634a8991142ca51ed5
Josh Maxwell <jmaxwell@quantizr.com>
2024-12-05 16:19:52 -0700

    other changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,9 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('Departure Mono', { weight = 'Thin' })
-config.font_size = 11.0
+--config.font = wezterm.font('Departure Mono', { weight = 'Thin' })
+config.font = wezterm.font('JetBrains Mono', { weight = 'Regular' })
+config.font_size = 13.0
 
 config.max_fps = 120
 config.window_close_confirmation = "NeverPrompt"
@@ -13,6 +14,9 @@ config.enable_tab_bar = false
 
 -- https://wezfurlong.org/wezterm/colorschemes/index.html
 config.color_scheme = 'Kanagawa (Gogh)'
+--config.color_scheme = 'Adventure'
+--config.color_scheme = 'Afterglow'
+--config.color_scheme = 'Alien Blood (Gogh)' --Looks awesome in nvim with the Kanagawa theme, I might need to combine those
 
 config.keys = {
   -- Turn off the default CMD-m Hide action, allowing CMD-m to
